9737d19fdc208189fcc6244cf7c4d31d7a4a3642
Joao Vitor Freitas <jvfreitas02@gmail.com>
2022-08-20 18:20:45 -0300

    Add front end pages, implementando as  rotas, cadastro de ongs, cadastro de caso, listagem de caso, remocao de caso, conectando API

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { api } from '../../services/api';
+
+import './styles.css';
+
+import { FiLogIn } from 'react-icons/fi';
+
+import homeImg from '../../assets/img-home.png';
+import logoImg from '../../assets/logo.svg';
+
+export function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha no login, tente novamente.');
+    }
+  }
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="logo" />
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+          <input
+            placeholder="sua ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#F34875" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={homeImg} alt="Heroes" />
+    </div>
+  );
+}
